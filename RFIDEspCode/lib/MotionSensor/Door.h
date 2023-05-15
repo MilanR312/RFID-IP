95e0b6c8eac49fc4f207b06ba66dd0d1873b4dbb
jorenvdbosch <127108328+jorenvdbosch@users.noreply.github.com>
2023-05-15 15:55:32 +0200

    motionsensor full fix

--- RFIDEspCode/lib/MotionSensor/Door.h
+++ RFIDEspCode/lib/MotionSensor/Door.h
@@ -5,18 +5,18 @@ extern tcpClient website;
 
 class Door
 {
-    MotionSensor m1;
-    MotionSensor m2;
+    MotionSensor m1; // sensor aan buitenkant
+    MotionSensor m2; // sensor aan binnenkant
 
     int deurSensPin;
     int deurPin;
     struct timeval tv_now;
-    struct timeval tim1;    // hoelang het duurt om deur te openen
-    struct timeval tim2;    // hoelang de deur open is
+    struct timeval tim1; // hoelang het duurt om deur te openen
+    struct timeval tim2; // hoelang de deur open is
     void (*callback)(void);
 
-    int gemiddelde_opening_time;    // in milliseconden
-    int aantal_openingen;           
+    int gemiddelde_opening_time; // in milliseconden
+    int aantal_openingen;
     int opening_time_sum;
 
     int64_t timevalue1;
@@ -29,7 +29,6 @@ public:
     bool open = false;
     bool isOpening = false;
     bool isClosing = false;
-    int tries = 0;
     bool forcedOpen = false;
     bool isAllowedEntry = false;
     bool tim1_running = false;
@@ -41,100 +40,136 @@ public:
         pinMode(deurSens, INPUT);
         pinMode(deur, OUTPUT);
     }
-    void loop(){
+    void loop()
+    {
         if (forcedOpen)
         {
             digitalWrite(deurPin, HIGH);
             return;
         }
+        /*
         if (!isAllowedEntry)
         {
             digitalWrite(deurPin, LOW);
             lcd.print("Geen toegang");
             return;
         }
+        */
+
         m1.sense();
         m2.sense();
 
-        if ((!digitalRead(m1.pin) || (!digitalRead(m2.pin) && isAllowedEntry)) && digitalRead(deurSensPin) == HIGH && !open) // wanneer iemand aant wachten is op opening vd deur
+        if ((!digitalRead(m1.pin) || (!digitalRead(m2.pin) && isAllowedEntry)) && digitalRead(deurSensPin) == LOW && !open && !isOpening) // wanneer iemand aant wachten is op opening vd deur
         {
             website.log("person detected at door");
             website.log("opening door");
             digitalWrite(deurPin, HIGH);
             isOpening = true;
-            if (!tim1_running){
+            if (!tim1_running)
+            {
+                website.log("timer 1 started");
+                isAllowedEntry = true;
                 gettimeofday(&tim1, NULL);
                 tim1_running = true;
+                lcd.clear();
                 lcd.print("De deur wordt geopend");
+                delay(1000);
             }
         }
 
-        else if (isOpening){
+        else if (isOpening)
+        {
             // zeker zijn dat de deur blijft openen
             digitalWrite(deurPin, HIGH);
             // tijd calculations uitvoeren
             gettimeofday(&tv_now, NULL);
             timevalue1 = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
             timevalue2 = (int64_t)tim1.tv_sec * 1000000L + (int64_t)tim1.tv_usec;
-            timedif = (timevalue1-timevalue2)/1000;
+            timedif = (timevalue1 - timevalue2) / 1000;
             // checken of de deur snel genoeg open ging
-            if (aantal_openingen > 10 && timedif > (gemiddelde_opening_time + 10000)){
-                if (digitalRead(m1.pin) || digitalRead(m2.pin)){
-                    website.send(LOG, "Deur openen duurde minstens 10 seconden langer dan verwacht: kijk deur na");
+            if (aantal_openingen > 10 && timedif > (gemiddelde_opening_time + 10000))
+            {
+                if (digitalRead(m1.pin) || digitalRead(m2.pin))
+                {
+                    website.send(LOG, "Deur openen duurde te lang");
                     website.send(ENABLED, "2");
                 }
             }
-            if (digitalRead(deurSensPin) == LOW){
+            if (digitalRead(deurSensPin) == HIGH)
+            {
                 // bools juist zetten
                 isOpening = false;
                 open = true;
                 tim1_running = false;
                 opening_time_sum += timedif;
                 aantal_openingen++;
-                gemiddelde_opening_time = opening_time_sum/aantal_openingen;
-                website.send(LOG, "Deur volledig geopend: start timer voor sluiten");
+                gemiddelde_opening_time = opening_time_sum / aantal_openingen;
+                website.log("Deur volledig geopend");
             }
         }
 
-        else if (open){
+        else if (open)
+        {
             // all time in microSecons
-            if (!tim2_running){
+            if (!tim2_running)
+            {
+                website.log("Timer 2 gestart");
                 gettimeofday(&tim2, NULL);
                 tim2_running = true;
+                lcd.clear();
                 lcd.print("De deur sluit binnen 15 seconden");
-            }
-
-            if (digitalRead(m1.pin) || digitalRead(m2.pin)){
-                gettimeofday(&tim2, NULL);
-                lcd.print("De deur sluit binnen 15 seconden");
+                delay(1000);
             }
 
             gettimeofday(&tv_now, NULL);
             timevalue1 = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
             timevalue2 = (int64_t)tim2.tv_sec * 1000000L + (int64_t)tim2.tv_usec;
-            timedif = (timevalue1-timevalue2)/1000;
+            timedif = (timevalue1 - timevalue2) / 1000;
 
+            if ((!digitalRead(m1.pin) || !digitalRead(m2.pin)) && timedif > 2500) // als een van de motion sensoren iets opvangt en na 5 sec
+            {
+                gettimeofday(&tim2, NULL);
+                website.log("Nieuwe persoon gescand");
+                lcd.clear();
+                lcd.print("De deur sluit binnen 15 seconden");
+                delay(1000);
+                website.log("stuck in loop?");
+                digitalWrite(deurPin, HIGH);
+                isClosing = false;
+            }
 
-            if (timedif > 15000){
+            if (timedif > 15000 && !isClosing)
+            {
                 website.send(LOG, "closing door");
+                lcd.clear();
                 lcd.print("Deur start met sluiten");
+                delay(1000);
                 digitalWrite(deurPin, LOW);
                 isClosing = true;
             }
 
-            if (timedif > (15000 + gemiddelde_opening_time + 10000)){
-                if (digitalRead(deurSensPin) == LOW){
-                    website.send(LOG, "Fatale error bij deur sluiten. Shutting down system");
+            if (timedif > (15000 + gemiddelde_opening_time + 10000))
+            {
+                if (digitalRead(deurSensPin) == HIGH)
+                {
+                    website.send(LOG, "Fatale error bij deur sluiten");
                     website.send(NAME, "Door malfunction");
                     website.send(ENABLED, "2");
+                    lcd.clear();
                     lcd.print("Error bij deur sluiten");
-                    while (true){}
+                    website.log("stuck in while");
+                    delay(1000);
+                    while (true)
+                    {
+                    }
                 }
-                else{
+                else
+                {
                     isAllowedEntry = false;
                     open = false;
+                    isClosing = false;
                     tim2_running = false;
-                    website.send(LOG, "Deur zou nu gesloten moeten zijn");
+                    website.send(LOG, "Deur gesloten");
                 }
             }
         }
--- RFIDEspCode/src/main.cpp
+++ RFIDEspCode/src/main.cpp
@@ -6,51 +6,51 @@
 #include "sql.h"
 #include "Door.h"
 #include <cstdio>
-const char * ssid = "IoTdevices";
-const char * password = "FGwrdsa=ghaR";
-
-
-
+const char *ssid = "IoTdevices";
+const char *password = "FGwrdsa=ghaR";
 
 #define RFID
 #define WEBSITE
 #define SQL
-//#define WRITE
-//buttonLink[0] -> doorOverride
+// #define WRITE
+// buttonLink[0] -> doorOverride
 Array<bool, 2> buttonLink;
 
 #ifdef WEBSITE
-tcpClient website("192.168.1.59", 8090);
+tcpClient website("172.22.31.194", 8090);
 #endif
 
 #ifdef SQL
-postgresESP sql(192,168,1,59,  5432);
+postgresESP sql(172, 22, 31, 194, 5432);
 Array<uint8_t, 4> read_buffer;
 #endif
 
-
 Unit_UHF_RFID rfid;
 
-Door dr(32,33,26,25);
-//rs en D4-D7
-LiquidCrystal lcd(4,18,19,21,22,23);
+Door dr(32, 33, 26, 25);
+// rs en D4-D7
+LiquidCrystal lcd(4, 18, 19, 21, 22, 23);
 
-//print text or new . and reset if > 16
-void printBar(const char * message1, uint8_t & num){
-  if (num == 0 || num > 16){
+// print text or new . and reset if > 16
+void printBar(const char *message1, uint8_t &num)
+{
+  if (num == 0 || num > 16)
+  {
     lcd.clear();
     lcd.print(message1);
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     num = 0;
-  } else {
-    lcd.setCursor(num,1);
+  }
+  else
+  {
+    lcd.setCursor(num, 1);
   }
   lcd.print(".");
 }
 
-
-//receive incomming message and execute
-void handleMessageV2(const char * message){
+// receive incomming message and execute
+void handleMessageV2(const char *message)
+{
   char buffer[33] = {0};
   char sendBuffer[32] = {};
   strcpy(buffer, message);
@@ -60,121 +60,133 @@ void handleMessageV2(const char * message){
   Serial.println(buffer[1]);
   char instr = buffer[0];
   int rw = (buffer[1] - '0');
-  if (rw == 0){ // read
-    switch(instr -'0'){
-      case NAME:
-        snprintf(sendBuffer, 32, "%c%-32s", instr, website.name.c_str());
-        website.send(sendBuffer);
-        break;
-      case LOCATION:
-        snprintf(sendBuffer, 32, "%c%-32s", instr, website.location.c_str());
-        website.send(sendBuffer);
-        break;
-      case LOGIN:
-        snprintf(sendBuffer, 32, "%c%-32s", instr, website.lastLoggedInUser);
-        website.send(sendBuffer);
-        break;
-      case BUTTON:{
-        //read data byte
-        int dev = buffer[2];
-        int buttonIndex = dev - '0';
-        Serial.print("index=");
-        Serial.println(buttonIndex);
-        int buttonData = buttonLink[buttonIndex];
-        Serial.print("data =");
-        Serial.println(buttonData);
-        snprintf(sendBuffer, 32, "%c%c%-31c", instr, (char)buttonIndex+'0', (char)buttonData+'0');
-        Serial.println(sendBuffer);
-        website.send(sendBuffer);
-        break;
-      }
-      case ENABLED:
-        website.send(ENABLED, "0");
-        break;
+  if (rw == 0)
+  { // read
+    switch (instr - '0')
+    {
+    case NAME:
+      snprintf(sendBuffer, 32, "%c%-32s", instr, website.name.c_str());
+      website.send(sendBuffer);
+      break;
+    case LOCATION:
+      snprintf(sendBuffer, 32, "%c%-32s", instr, website.location.c_str());
+      website.send(sendBuffer);
+      break;
+    case LOGIN:
+      snprintf(sendBuffer, 32, "%c%-32s", instr, website.lastLoggedInUser);
+      website.send(sendBuffer);
+      break;
+    case BUTTON:
+    {
+      // read data byte
+      int dev = buffer[2];
+      int buttonIndex = dev - '0';
+      Serial.print("index=");
+      Serial.println(buttonIndex);
+      int buttonData = buttonLink[buttonIndex];
+      Serial.print("data =");
+      Serial.println(buttonData);
+      snprintf(sendBuffer, 32, "%c%c%-31c", instr, (char)buttonIndex + '0', (char)buttonData + '0');
+      Serial.println(sendBuffer);
+      website.send(sendBuffer);
+      break;
     }
-  } else {
-    switch(instr - '0'){
-      case BUTTON:
-        int index = buffer[2];
-        int newState = buffer[3];
-        Serial.println("setting button");
-        buttonLink[index - '0'] = newState - '0';
-        snprintf(sendBuffer, 32, "%c%c%-31c", instr, (char)index, (char)newState);
-        website.send(sendBuffer);
-        //index = 0 && newState == 1 => red (2)
-        //index = 1 && newState == 1 => yellow (1)
-        Serial.print("buttons:");
-        Serial.print(buttonLink[0]);
-        Serial.print(" ");
-        Serial.println(buttonLink[1]);
-        if (buttonLink[0])
-          website.send(ENABLED, "2");
-        else if (buttonLink[1])
-          website.send(ENABLED, "1");
-        else
-          website.send(ENABLED, "0");
+    case ENABLED:
+      website.send(ENABLED, "0");
+      break;
+    }
+  }
+  else
+  {
+    switch (instr - '0')
+    {
+    case BUTTON:
+      int index = buffer[2];
+      int newState = buffer[3];
+      Serial.println("setting button");
+      buttonLink[index - '0'] = newState - '0';
+      snprintf(sendBuffer, 32, "%c%c%-31c", instr, (char)index, (char)newState);
+      website.send(sendBuffer);
+      // index = 0 && newState == 1 => red (2)
+      // index = 1 && newState == 1 => yellow (1)
+      Serial.print("buttons:");
+      Serial.print(buttonLink[0]);
+      Serial.print(" ");
+      Serial.println(buttonLink[1]);
+      if (buttonLink[0])
+        website.send(ENABLED, "2");
+      else if (buttonLink[1])
+        website.send(ENABLED, "1");
+      else
+        website.send(ENABLED, "0");
     }
   }
 }
 
-//tx = 14 , rx = 12
+// tx = 14 , rx = 12
 
-void checkUser(char * buff){
-  #ifdef SQL
+void checkUser(char *buff)
+{
+#ifdef SQL
 
-  if(char * user = sql.checkUser(buff)){
-    //copy name into lastloggedin    
+  if (char *user = sql.checkUser(buff))
+  {
+    // copy name into lastloggedin
     strncpy(website.lastLoggedInUser, user, 32);
 
     website.send(LOGIN, website.lastLoggedInUser);
 
     lcd.clear();
     lcd.print(user);
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("opened the door");
     website.log(user);
-    if(!dr.forcedOpen)
+    if (!dr.forcedOpen)
       dr.isAllowedEntry = true;
-  } else {
+  }
+  else
+  {
     Serial.println("no entry");
     lcd.clear();
     lcd.print("no entry");
   }
 
-  #else //als SQL server niet verbonden is laat iedereen toe
+#else // als SQL server niet verbonden is laat iedereen toe
   if (!dr.forcedOpen)
     dr.isAllowedEntry = true;
-    
-  #endif
+
+#endif
   delay(2000);
 }
-void scanArtikel(char * buff){
-  #ifdef SQL
+void scanArtikel(char *buff)
+{
+#ifdef SQL
   int direction = dr.getDirection();
   char bf[25];
   snprintf(bf, 25, "product %s going %d", buff, direction);
   website.log(bf);
-  if (direction != 0){
+  if (direction != 0)
+  {
     Serial.println("writing to dbs");
-    sql.setArtikel(buff, direction > 0);
+    sql.setArtikel(buff, direction < 0);
 
-    delay(5000); //wait 5 seconds to let product get out of radius
-    //fix this !! bad solution
+    delay(5000); // wait 5 seconds to let product get out of radius
+    // fix this !! bad solution
   }
-  #endif
+#endif
 }
 
-void setup() {
+void setup()
+{
   delay(5000);
   Serial.begin(115200);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.clear();
   lcd.home();
-  pinMode(6, OUTPUT);
-  digitalWrite(6, LOW);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, LOW);
 
   lcd.print("starting up");
-
 
   delay(1000);
   lcd.clear();
@@ -186,64 +198,63 @@ void setup() {
   uint8_t tryAmount = 0;
 
   printBar("wifi connect", tryAmount);
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println(WiFi.status());
     tryAmount++;
-    printBar("wifi connect", tryAmount);    
+    printBar("wifi connect", tryAmount);
     delay(500);
-    
   }
   lcd.clear();
   lcd.print("Wifi connected");
   delay(1000);
-  #ifdef RFID
+#ifdef RFID
   lcd.clear();
   lcd.print("connect rfid now");
   delay(5000);
 
-  //rx tx
+  // rx tx
   rfid.begin(&Serial2, 115200, 14, 12, false);
-  
+
   String info;
   tryAmount = 0;
   printBar("RFID connect", tryAmount);
-  while (1){
+  while (1)
+  {
     info = rfid.getVersion();
     Serial.println(info);
     tryAmount++;
     printBar("RFID connect", tryAmount);
     if (info != "ERROR")
-      break;  
+      break;
     delay(500);
   }
   lcd.clear();
   lcd.print("RFID connected");
   delay(2000);
   rfid.setTxPower(100);
-  #endif
+#endif
 
-
-
-  #ifdef WEBSITE
+#ifdef WEBSITE
   website.connect();
   website.name = "ESP32Test";
   website.location = "testingLocation";
   website.registerCallback(handleMessageV2);
-  #endif
+#endif
 
-  #ifdef SQL
+#ifdef SQL
   sql.connect("ESP32", "ESP32", "RFID");
-  #endif
+#endif
 
   lcd.clear();
 
-  #ifdef WRITE
-
+#ifdef WRITE
 
   lcd.clear();
   lcd.print("writing in");
-  lcd.setCursor(0,1);
-  for (int i = 3; i > 0; i--){
+  lcd.setCursor(0, 1);
+  for (int i = 3; i > 0; i--)
+  {
     lcd.print(i);
     delay(1000);
   }
@@ -251,77 +262,85 @@ void setup() {
   write_buffer[3] = '1';
   write_buffer[2] = '0';
   write_buffer[1] = '0';
-  write_buffer[0]  = 0b10000000; //user starts with 1
-  //hardcode for program to chip
+  write_buffer[0] = 0b10000000; // user starts with 1
+  // hardcode for program to chip
   lcd.clear();
   if (rfid.writeCard(write_buffer, sizeof(write_buffer), 0x04, 0,
-                          0x00000000)) {
-      lcd.print("write Succesfull");
-  } else {
-      lcd.print("Write ERROR");
+                     0x00000000))
+  {
+    lcd.print("write Succesfull");
+  }
+  else
+  {
+    lcd.print("Write ERROR");
   }
   delay(1000);
-  #endif
+#endif
   lcd.clear();
   lcd.print("starting main loop");
   delay(1000);
 }
 
-void loop() {
-  #ifdef WEBSITE
+void loop()
+{
+#ifdef WEBSITE
 
-  do {
+  do
+  {
     website.mainLoop();
     dr.loop();
-    if (buttonLink[1]){ //if esp is turned off
+    if (buttonLink[1])
+    { // if esp is turned off
       lcd.clear();
       lcd.print("Door disabled");
-      
+
       delay(500);
     }
-    else if (buttonLink[0]){ //if door forced open
+    else if (buttonLink[0])
+    { // if door forced open
       dr.forcedOpen = true;
       strncpy(website.lastLoggedInUser, "OVERRIDE", 32);
 
       lcd.clear();
       lcd.print("forced open");
-      //website.send(LOGIN, website.lastLoggedInUser);
+      // website.send(LOGIN, website.lastLoggedInUser);
 
       delay(1000);
     }
   } while (buttonLink[1] || buttonLink[0]); // turns off the esp32 (kinda)
-  
+
   dr.forcedOpen = false;
   lcd.clear();
 
-  #endif
+#endif
 
-
-
-  #ifdef RFID
-  //read a tag if not forced open
-  if (rfid.readCard(read_buffer.pdata(), read_buffer.length(), 0x04, 0, 0)){
+#ifdef RFID
+  // read a tag if not forced open
+  if (rfid.readCard(read_buffer.pdata(), read_buffer.length(), 0x04, 0, 0))
+  {
     Serial.println("read succesfull");
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++)
+    {
       Serial.print(read_buffer[i]);
       Serial.print(" ");
     }
-    //card scanned is an user
+    // card scanned is an user
     char buff[4];
     snprintf(buff, 4, "%c%c%c", read_buffer[1], read_buffer[2], read_buffer[3]);
-    if (read_buffer[0] >> 7 == 1){
+    if (read_buffer[0] >> 7 == 1)
+    {
       Serial.println("user scanned checking dbs");
-      website.log("user has scanned card");      
+      website.log("user has scanned card");
       checkUser(buff);
-
     }
-    //card scanned is a product
-    if (dr.open && read_buffer[0] >> 7 == 0){
+    // card scanned is a product
+    if (dr.open && read_buffer[0] >> 7 == 0)
+    {
       Serial.println("product scanned getting direction");
       scanArtikel(buff);
     }
   }
-  #endif
+#endif
 
-  delay(1000);
+  delay(50);
 }
